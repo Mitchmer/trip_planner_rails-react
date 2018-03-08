3a57b583a07f452d1f534724c0474b4b2059ebba
Shandelle Lovato <ShandelleLovato@gmail.com>
2018-03-08 15:29:11 -0700

    adds AddressForm.js

---
+++ client/src/component/AddressForm.js
@@ -0,0 +1,44 @@
+import React from 'react'
+
+class AddressForm extends React.Component {
+  // state = { Latitude: '', Longitude: '' }
+  state = { title: '' }
+
+  handleChange = (e) => {
+    const { name, value } = e.target
+    this.setState({ [name]: value })
+  }
+
+  handleSubmit = (e) => {
+    e.preventDefault()
+    this.props.addItem(this.state.title)
+    this.setState({ title: '' })
+    // this.props.addItem(this.state.Latitude)
+    // this.props.addItem(this.state.Longitude)
+    // this.setState({ Latitude: '' })
+    // this.setState({ Longitude: '' })
+  }
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <input
+          placeholder="Latitude"
+          required
+          value={this.state.Latitude}
+          onChange={this.handleChange}
+          name="Latitude"
+        />
+        <input
+          placeholder="Longitude"
+          required
+          value={this.state.Longitude}
+          onChange={this.handleChange}
+          name="Longitude"
+        />
+        </form>
+      )
+    }
+  }
+
+export default AddressForm
