39542639d17ae65ab6cb27d9156903c0996c0f1e
Andrew Street <andrew_street@live.com>
2018-03-08 16:22:36 -0700

    accepted imcoming changes

--- client/src/components/Trip.js
+++ client/src/components/Trip.js
@@ -2,11 +2,6 @@ import React from 'react';
 
 const Trip = ({ id, name, deleteTrip }) => (
   <div>
-<<<<<<< HEAD
-    <div>{name}</div>
-    <button onClick={() => deleteTrip(id)}>Delete</button>
-  </div>
-=======
     <div>
       {name}
     </div>
@@ -15,7 +10,6 @@ const Trip = ({ id, name, deleteTrip }) => (
     </button>
   </div>
 
->>>>>>> changed render
   // <button onClick={() => addLocation(id)}>
   //   Add Location
   // </button>
